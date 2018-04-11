dcefd906e35900abb4dfaf694efe6bbd48010362
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-04-11 17:15:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,23 @@ const createFareMultiplier = function (multiplier) {
  };
 };
 
-//fareDoubler() — Declare a variable with const and assign a function returned by createFareMultiplier() to it. Invoke createFareMultiplier() in such a way that the new fareDoubler() function 
+//fareDoubler() — Declare a variable with const and assign a function returned by createFareMultiplier() to it. 
+//Invoke createFareMultiplier() in such a way that the new fareDoubler() function
 //accepts a fare as its lone argument and doubles it.
-//doubles fares 
+//doubles fares
 const fareDoubler = createFareMultiplier(2);
+
+//fareTripler() — Declare a variable with const and assign a function returned by createFareMultiplier() to it.
+// Invoke createFareMultiplier() in such a way that the new fareTripler() function 
+//accepts a fare as its lone argument and triples it.
+//s a function 
+
+//ReferenceError: 'fareTripler' is not defined
+  // at Anonymous function (test/indexTest.js:72:7)
+//triples fares 
+
+//ReferenceError: 'fareTripler' is not defined
+// at Anonymous function (test/indexTest.js:76:7)
+
+const fareTripler = createFareMultiplier(3);
+
