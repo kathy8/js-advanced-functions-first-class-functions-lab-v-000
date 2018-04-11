3e431d412d2cf4e6798e93a21ee94b27c331189c
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-04-11 17:17:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,23 +32,32 @@ const createFareMultiplier = function (multiplier) {
  };
 };
 
-//fareDoubler() — Declare a variable with const and assign a function returned by createFareMultiplier() to it. 
+//fareDoubler() — Declare a variable with const and assign a function returned by createFareMultiplier() to it.
 //Invoke createFareMultiplier() in such a way that the new fareDoubler() function
 //accepts a fare as its lone argument and doubles it.
 //doubles fares
 const fareDoubler = createFareMultiplier(2);
 
 //fareTripler() — Declare a variable with const and assign a function returned by createFareMultiplier() to it.
-// Invoke createFareMultiplier() in such a way that the new fareTripler() function 
+// Invoke createFareMultiplier() in such a way that the new fareTripler() function
 //accepts a fare as its lone argument and triples it.
-//s a function 
+//s a function
 
 //ReferenceError: 'fareTripler' is not defined
   // at Anonymous function (test/indexTest.js:72:7)
-//triples fares 
+//triples fares
 
 //ReferenceError: 'fareTripler' is not defined
 // at Anonymous function (test/indexTest.js:76:7)
 
 const fareTripler = createFareMultiplier(3);
+
+//selectDifferentDrivers() — This function takes two arguments, an array of drivers and either the returnFirstTwoDrivers() or returnLastTwoDrivers() function. Based on these two arguments, selectDifferentDrivers() will return 
+//either the first two drivers or the last two drivers.
+//returns the first two drivers when passed returnFirstTwoDrivers() as the second argument 
+//returns the last two drivers when passed returnLastTwoDrivers() as the second argument 
+
+const selectDifferentDrivers = function (array,list) {
+  return list(array);
+};
 
